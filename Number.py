d36f97bb6ad1b898c8dd8fccf9d169d09a0562c0
Simon <39882168+UserName0299@users.noreply.github.com>
2019-01-12 23:18:46 +0100

    Add files via upload

---
+++ Number.py
@@ -0,0 +1,14 @@
+variable_eins = 120
+variable_zwei = 312
+
+
+def geteilt_durch_zwei(number1):
+    number1 = number1 / 2
+    return number1
+
+
+print(geteilt_durch_zwei(variable_eins))
+print(geteilt_durch_zwei(variable_zwei))
+
+# Python is amazing :))
+
